bc98e42c30ddf7520a32eaf7f5f4f76c41936c40
Jackson Romie <jromie0924@gmail.com>
2022-03-29 16:28:15 -0600

    update config

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ class Config():
   def __init__(self) -> None:
       self.mq_username=''
       self.mq_password=''
-      self.mq_url='192.168.1.27'
+      self.mq_url='192.168.4.2'
       self.mq_port='5672'
       self.mq_emergency_exchange='Emergency'
       self.mq_emergency_queue='EmergencyCommands'
+      self.SHUTDOWN_BODY = 'SHUTDOWN'
